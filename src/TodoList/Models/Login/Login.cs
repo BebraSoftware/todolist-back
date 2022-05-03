3adb1878b0554f908ce934577e2657bfe8ce1cfe
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-05-03 13:36:57 +0700

    Add Login model

---
+++ src/TodoList/Models/Login/Login.cs
@@ -0,0 +1,13 @@
+﻿namespace BebraSoftware.TodoList.Models.Login
+{
+    public class Login
+    {
+        #region Fields
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        #endregion
+    }
+}
