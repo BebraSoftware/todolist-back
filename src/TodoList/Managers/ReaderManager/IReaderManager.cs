358ce2ff5141958660f32844227dea374442a9d0
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-05-05 20:01:46 +0700

    Add Reader/Writer manager

---
+++ src/TodoList/Managers/ReaderManager/IReaderManager.cs
@@ -0,0 +1,11 @@
+﻿namespace BebraSoftware.TodoList.Managers.ReaderManager
+{
+    public interface IReaderManager<out T, in TInfo>
+    {
+        #region Methods
+
+        T Read(TInfo info);
+
+        #endregion
+    }
+}
---
+++ src/TodoList/Managers/WriterManager/IWriterManager.cs
@@ -0,0 +1,11 @@
+﻿namespace BebraSoftware.TodoList.Managers.WriterManager
+{
+    public interface IWriterManager<in T, in TInfo>
+    {
+        #region Methods
+
+        bool Write(T obj, TInfo info);
+
+        #endregion
+    }
+}
