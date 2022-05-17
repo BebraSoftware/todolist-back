24a1416e8d73fc8cb7deed8e360067412c1c059e
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-05-18 04:37:04 +0700

    feat: user authenticate/registrate

---
+++ src/TodoList/Models/Login/LoginResponseModel.cs
@@ -0,0 +1,30 @@
+﻿namespace BebraSoftware.TodoList.Models.Login
+{
+    using System;
+
+    using Models.User;
+
+    public class LoginResponseModel
+    {
+        #region Properties
+
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public LoginResponseModel(RegularUser user, string token)
+        {
+            Id = user.Id;
+            Username = user.Username;
+            Token = token;
+        }
+
+        #endregion
+    }
+}
